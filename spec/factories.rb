6eb46ab80928207194f7da4f74081daf761bf8cd
Bravo <jbravo@deloitte.pt>
2021-11-03 20:22:01 +0000

    Fixed problem with specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
   factory :video do
     category
     sequence(:title) { |n| "Title_#{n}" }
+    file { Rack::Test::UploadedFile.new('spec/mocks/video_mock_1.mp4', 'video/mp4')  }
   end
 end
